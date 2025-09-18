b8edb7768cfa88e9f5c072d5eb1d0b01e8448b6f
ippapi <coricutiphong@gmail.com>
2025-09-18 16:05:11 +0700

    change logging method

--- databricks/pretrain_model/main.py
+++ databricks/pretrain_model/main.py
@@ -1,42 +1,37 @@
 import argparse
 import pandas as pd
 import time
-import logging
 from surprise import Dataset, Reader, SVD
 from surprise import dump
 from surprise import accuracy
-from collections import defaultdict
+import sys
 
-import logging
+class FileLogger:
+    def __init__(self, logfile=None):
+        self.logfile = logfile
 
-class CustomLogger:
-    def __init__(self, logfile=None, level=logging.INFO):
-        self.logger = logging.getLogger("custom_logger")
-        self.logger.setLevel(level)
-        if not self.logger.handlers:
-            ch = logging.StreamHandler()
-            ch.setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%H:%M'))
-            self.logger.addHandler(ch)
-            if logfile:
-                fh = logging.FileHandler(logfile)
-                fh.setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%H:%M'))
-                self.logger.addHandler(fh)
+    def _log(self, level, msg):
+        t = time.strftime("%H:%M", time.localtime())
+        line = f"[{t}][{level}] {msg}\n"
+        if self.logfile:
+            with open(self.logfile, "a") as f:
+                f.write(line)
+        sys.stdout.write(line)
+        sys.stdout.flush()
+
     def info(self, msg):
-        self.logger.info(msg)
-        self.flush()
+        self._log("INFO", msg)
+
     def warning(self, msg):
-        self.logger.warning(msg)
-        self.flush()
+        self._log("WARNING", msg)
+
     def error(self, msg):
-        self.logger.error(msg)
-        self.flush()
-    def flush(self):
-        for h in self.logger.handlers:
-            h.flush()
+        self._log("ERROR", msg)
 
 def main(args):
-    log = CustomLogger(args.log_path)
-    logging.info("Loading data...")
+    log = FileLogger(args.log_path)
+
+    log.info("Loading data...")
     if args.traindir:
         train_df = pd.read_parquet(args.traindir)
 
@@ -48,39 +43,39 @@ def main(args):
     trainset = train_data.build_full_trainset()
 
     if args.load_model:
-        logging.info(f"Loading model from {args.load_model} ...")
+        log.info(f"Loading model from {args.load_model} ...")
         algo, _ = dump.load(args.load_model)
     else:
-        algo = SVD(n_factors=20, n_epochs=15, reg_all=0.1)
+        algo = SVD(n_factors=args.n_factor, n_epochs=args.n_epoch, reg_all=args.reg_all)
 
     start_time = time.time()
-    logging.info("Training model...")
+    log.info("Training model...")
     algo.fit(trainset)
-    logging.info(f"Training done in {time.time() - start_time:.2f}s")
+    log.info(f"Training done in {time.time() - start_time:.2f}s")
 
     if args.save_model:
-        logging.info(f"Saving model to {args.save_model} ...")
+        log.info(f"Saving model to {args.save_model} ...")
         dump.dump(args.save_model, algo=algo)
 
-    if args.mode == "eval" and not test_df.empty:
-        logging.info("Evaluating model...")
+    if args.mode == "eval" and args.testdir:
+        log.info("Evaluating model...")
         testset = list(zip(test_df['user_id'], test_df['movie_id'], test_df['rating']))
         predictions = algo.test(testset)
         rmse = accuracy.rmse(predictions)
-        logging.info(f"RMSE on eval set: {rmse:.4f}")
+        log.info(f"RMSE on eval set: {rmse:.4f}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--traindir", type = str, required=True)
-    parser.add_argument("--testdir", type = str)
-    parser.add_argument("--save_model", type = str, default=None)
-    parser.add_argument("--load_model", type = str, default=None)
-    parser.add_argument("--log_path", type = str)
+    parser.add_argument("--traindir", type=str, required=True)
+    parser.add_argument("--testdir", type=str)
+    parser.add_argument("--save_model", type=str, default=None)
+    parser.add_argument("--load_model", type=str, default=None)
+    parser.add_argument("--log_path", type=str, default=None)
     parser.add_argument("--mode", type=str, default="train", choices=["train","eval"])
-    parser.add_argument("--n_factor", default = 20, type = int)
-    parser.add_argument("--n_epoch", default = 15, type = int)
-    parser.add_argument("--rag_all", default = 0.1, type = float)
+    parser.add_argument("--n_factor", type=int, default=20)
+    parser.add_argument("--n_epoch", type=int, default=15)
+    parser.add_argument("--reg_all", type=float, default=0.1)
 
     args = parser.parse_args()
-
     main(args)
+
