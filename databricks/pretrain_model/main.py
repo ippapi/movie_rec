ad65fe16dea322297d2223ae4e7c54ea4bf4ff0f
ippapi <coricutiphong@gmail.com>
2025-09-18 17:25:19 +0700

    forget to import model

--- databricks/pretrain_model/main.py
+++ databricks/pretrain_model/main.py
@@ -1,7 +1,7 @@
 import argparse
 import pandas as pd
 import time
-from surprise import Dataset, Reader, SVD
+from surprise import Dataset, Reader, SVD, NMF, KNNWithMeans
 from surprise import dump
 from surprise import accuracy
 import sys
