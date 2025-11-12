e509d33505b7b482b322d7b92be05c4625b9038b
Vũ Thanh Phong <116993591+ippapi@users.noreply.github.com>
2025-11-12 19:22:56 +0700

    Update main.py

--- dsvm/pretrain_model/main.py
+++ dsvm/pretrain_model/main.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--traindir", type=str)
     parser.add_argument("--testdir", type=str)
-    parser.add_argument("--model_name", default="svd", choices=["svd", "nmf", "knn"])
+    parser.add_argument("--model_name", default="svd", choices=["svd", "nmf", "knn", "baseline", "slopeone", "cocluster"])
     parser.add_argument("--save_model", type=str, default=None)
     parser.add_argument("--load_model", type=str, default=None)
     parser.add_argument("--log_path", type=str, default=None)
